af14d80d0b92e951c3e4870a54f56e26dcfd1a3d
Darko Kuzmanovski <prijatelce@yahoo.com>
2017-09-14 23:28:53 +0200

    start the 'year' tab implementation

--- src/pages/godina/godina.ts
+++ src/pages/godina/godina.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { PrazniciProvider } from '../../providers/praznici/praznici';
+
 @Component({
   selector: 'page-godina',
   templateUrl: 'godina.html'
 })
 export class GodinaPage {
-  year = '';
-  constructor(public navCtrl: NavController) {
-    this.year = new Date().getFullYear().toString();
+  year = new Date().getFullYear();
+  prazniciList = [];
+  constructor(public navCtrl: NavController, private praznici: PrazniciProvider) {
+    this.loadPraznici(this.year);
+  }
+
+  public loadPraznici(year) {
+
   }
 
 }
